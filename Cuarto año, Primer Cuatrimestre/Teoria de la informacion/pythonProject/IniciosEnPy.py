03cb3ee20ca5f01dc99269a7e5e259c116446503
Florencia Piu <flor.piuu@gmail.com>
2023-05-12 00:12:40 -0300

    Agregando materia de cuarto

---
+++ Cuarto año, Primer Cuatrimestre/Teoria de la informacion/pythonProject/IniciosEnPy.py
@@ -0,0 +1,56 @@
+#List
+a = [10,20,30]
+['Hello', 'Bye']
+[10,"Hello", True, 10.1]
+[]
+
+#Tuples
+b = (10,20,30)
+()
+#print(type(b)) #Saber tipo de dato
+
+#Dictorionies (clave id, valor)
+c={
+    "nombre de la persona": "Ryan",
+    "lastname":"Ray",
+    "nickname": "Roro"
+}
+#-----------------------------------
+#case sensitive (sensible a may y min)
+book = "libro1"
+Book = "libro2"
+book, Book = "libro1", "libro2"
+
+# las constantes se escriben en mayusculas
+PI = 3.1415
+
+#Lenguaje de tipiado dinamico
+book = "libro1"
+book = 1111
+#------------------------------------
+#STRING
+# mystr = "Hello World"
+# print(mystr.upper()) #MAY
+# print(mystr.lower()) #min
+# print(mystr.swapcase()) #cambia del orginal
+# print(mystr.capitalize()) #Primer palabra en may
+# print(mystr.replace('Hello', 'bye').upper()) #Cambiar una plabra y poner todo en may
+# print(mystr.count('l')) #Contar cantidad de letras l
+# print(mystr.startswith('Hello')) #Si empieza con determinada palabra/letra
+# print(mystr.endswith('World')) #Si termina con determinada palabra/letra
+# print(mystr.split()) #Separa la palabra por caracter en blaco por defecto
+# print(mystr.find('d')) #Posicion del caracter
+# print(len(mystr)) #Longitud del String
+# print(mystr.index('e'))
+# print(mystr.isnumeric())
+# print(mystr.isalpha())
+# print(mystr[4]) #Que caracter se encuentra en esa posicion
+# print(mystr[-1]) #Orden inverso
+
+# mystr = "Florencia"
+# print(mystr + " Piu") #Concatenar
+# print(f"My name is {mystr}") #f es para decir que voy a usar una variable creada anteriormente
+
+#-----------------------------------------------------
+#NUMERICO
+print(3//<2)
